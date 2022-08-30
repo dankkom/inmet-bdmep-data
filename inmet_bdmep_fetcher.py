eadb8edd6520aed0fc9fb6224e83c29548d37e87
Daniel Komesu <danielkomesu@gmail.com>
2022-08-30 17:49:15 +0000

    👩‍🏫 Incorporate fetcher.py code inside module

--- inmet_bdmep_fetcher.py
+++ inmet_bdmep_fetcher.py
@@ -57,3 +57,47 @@ def download_year(
             f.write(data)
             pbar.update(len(data))
     pbar.close()
+
+
+def cli():
+
+    import argparse
+
+    def expand_years(*years: str) -> list[int]:
+        year_list = []
+        for y in years:
+            if ":" in y:
+                start, end = y.split(":")
+                year_list.extend(list(range(int(start), int(end) + 1)))
+            else:
+                year_list.append(int(y))
+        return year_list
+
+    def get_args():
+        parser = argparse.ArgumentParser(
+            description="Download INMET BDMEP data",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        )
+        parser.add_argument(
+            "years",
+            nargs="+",
+            help="Years to download",
+        )
+        parser.add_argument(
+            "--datadir",
+            dest="datadir",
+            type=pathlib.Path,
+            required=True,
+            help="Destination directory",
+        )
+        return parser
+
+    args = get_args()
+    datadir = args.datadir
+    years = expand_years(*args.years)
+    for year in years:
+        download_year(year, datadir)
+
+
+if __name__ == "__main__":
+    cli()
