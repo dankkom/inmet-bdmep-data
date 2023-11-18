32d2db82f97cad52b3f1d09f4b3b1ffca0964ff9
Daniel Komesu <danielkomesu@gmail.com>
2023-11-17 21:34:24 -0300

    Refactor script

--- run.py
+++ run.py
@@ -1,42 +1,42 @@
+import argparse
 from pathlib import Path
 
 from inmet_bdmep.fetcher import download_year
 
 
-def cli():
+def expand_years(*years: str) -> list[int]:
+    year_list = []
+    for y in years:
+        if ":" in y:
+            start, end = y.split(":")
+            year_list.extend(list(range(int(start), int(end) + 1)))
+        else:
+            year_list.append(int(y))
+    return year_list
 
-    import argparse
 
-    def expand_years(*years: str) -> list[int]:
-        year_list = []
-        for y in years:
-            if ":" in y:
-                start, end = y.split(":")
-                year_list.extend(list(range(int(start), int(end) + 1)))
-            else:
-                year_list.append(int(y))
-        return year_list
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Download INMET BDMEP data",
+    )
+    parser.add_argument(
+        "years",
+        nargs="+",
+        help="Years to download",
+    )
+    parser.add_argument(
+        "-o",
+        "--data-dir",
+        dest="data_dir",
+        type=Path,
+        required=True,
+        help="Destination directory",
+    )
+    args = parser.parse_args()
+    return args
 
-    def get_args():
-        parser = argparse.ArgumentParser(
-            description="Download INMET BDMEP data",
-        )
-        parser.add_argument(
-            "years",
-            nargs="+",
-            help="Years to download",
-        )
-        parser.add_argument(
-            "-o",
-            "--data-dir",
-            dest="data_dir",
-            type=Path,
-            required=True,
-            help="Destination directory",
-        )
-        args = parser.parse_args()
-        return args
 
+def main():
     args = get_args()
     datadir = args.datadir
     years = expand_years(*args.years)
@@ -45,4 +45,4 @@ def cli():
 
 
 if __name__ == "__main__":
-    cli()
+    main()
